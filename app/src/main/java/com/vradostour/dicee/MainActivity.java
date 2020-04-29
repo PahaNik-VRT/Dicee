b2e3bed55ac7b1ef63df5518d047bdb485757ba6
Pahanik-VRT <nikitapa@rambler.ru>
2020-04-29 13:25:39 +0300

    Dicee

---
+++ app/src/main/java/com/vradostour/dicee/MainActivity.java
@@ -0,0 +1,44 @@
+package com.vradostour.dicee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = findViewById(R.id.rollbutton);
+
+        final ImageView leftDice = findViewById(R.id.image_leftDice);
+        final ImageView rightDice = findViewById(R.id.image_rightDice);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Dicee", "Кнопишшу нажали!");
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(6);
+
+                Log.d("Dicee", "Рандомный номер:" + number);
+                final int[] diceArray = {R.drawable.dice1, R.drawable.dice2,
+                        R.drawable.dice3, R.drawable.dice4,
+                        R.drawable.dice5, R.drawable.dice6};
+                leftDice.setImageResource(diceArray[number]);
+                number = randomNumberGenerator.nextInt(6);
+                rightDice.setImageResource(diceArray[number]);
+
+            }
+        });
+    }
+}
